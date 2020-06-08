d7f70b6195ee99ce86ade26a6eebad88a294a904
koga yushi <youshe.old.pleasure@gmail.com>
2020-06-08 21:06:31 +0900

    initial comimt

---
+++ src/main/kotlin/selfsignedcertificate/SelfsignedcertificateApplication.kt
@@ -0,0 +1,11 @@
+package selfsignedcertificate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringApplication
+
+@SpringBootApplication
+class SelfsignedcertificateApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(SelfsignedcertificateApplication::class.java, *args)
+}
---
+++ src/test/kotlin/selfsignedcertificate/SelfsignedcertificateApplicationTests.kt
@@ -0,0 +1,13 @@
+package selfsignedcertificate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfsignedcertificateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
